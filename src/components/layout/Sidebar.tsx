5d81f85915b07dc082d4754a164d61fa4a84f4e6
antek <antoniswiderek@gmail.com>
2026-07-30 00:49:29 +0200

    add icons

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 
 const normalItems = [
-  { name: "Dashboard", path: "/dashboard", icon: "🏠" },
-  { name: "Mapa", path: "/map", icon: "🗺️" },
-  { name: "Misje", path: "/missions", icon: "🎯" },
-  { name: "Adopcje", path: "/adoptions", icon: "🐱" },
-  { name: "Fundacje", path: "/foundations", icon: "❤️" },
-  { name: "Ranking", path: "/ranking", icon: "🏆" },
-  { name: "Profil", path: "/profile", icon: "👤" },
+  { name: "Dashboard", path: "/dashboard", icon: "dashboard" },
+  { name: "Mapa", path: "/map", icon: "mapa" },
+  { name: "Misje", path: "/missions", icon: "misje" },
+  { name: "Adopcje", path: "/adoptions", icon: "adopcje" },
+  { name: "Fundacje", path: "/foundations", icon: "fundacje" },
+  { name: "Ranking", path: "/ranking", icon: "ranking" },
+  { name: "Profil", path: "/profile", icon: "profil" },
 ];
 
 export default function Sidebar() {
@@ -16,16 +16,17 @@ export default function Sidebar() {
   
   const items =
     admins.includes(user?.email)
-      ? [...normalItems, { name: "Admin", path: "/admin", icon: "🛠️" }]
+      ? [...normalItems, { name: "Admin", path: "/admin", icon: "admin" }]
       : normalItems;
+  const dark = localStorage.theme === 'dark' || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
 
   return (
-    <aside className="w-72 bg-overlay-base p-8 dark:bg-base" style={{ height: '100%', borderRight: `2px solid var(--color-${localStorage.theme === 'dark' || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches) ? 'overlay' : 'light-overlay'})` }}>
+    <aside className="w-72 bg-overlay-base p-8 dark:bg-base" style={{ height: '100%', borderRight: `2px solid var(--color-${dark ? 'overlay' : 'light-overlay'})` }}>
       <Link
         to="/dashboard"
       >
         <img
-          src={localStorage.theme === 'dark' || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches) ? '/logo_dark.svg' : '/logo_light.svg'}
+          src={dark ? '/logo_dark.svg' : '/logo_light.svg'}
           width='75%'
           className="mx-auto mb-8 cursor-pointer"
         />
@@ -37,9 +38,12 @@ export default function Sidebar() {
           <Link
             key={item.name}
             to={item.path}
-            className={`btn flex gap-3 rounded-2xl px-5 py-4 transition hover:text-light-border dark:hover:text-border ${window.location.pathname == item.path ? 'bg-[#896a3650] dark:bg-[#c15a15] text-light-text dark:text-sidebar font-bold' : 'text-light-subtext dark:text-subtext hover:bg-light-overlay dark:hover:bg-overlay' }`}
+            className={`btn flex gap-3 rounded-2xl px-5 py-3 transition hover:text-light-border leading-10 ${window.location.pathname == item.path ? 'bg-[#896a3650] dark:bg-[#c15a15] text-light-text dark:text-sidebar font-bold dark:hover:text-subtext' : 'text-light-subtext dark:text-subtext hover:bg-light-overlay dark:hover:bg-overlay dark:hover:text-border' }`}
           >
-            <span>{item.icon}</span>
+            <img 
+              src={`/icon_${dark ? 'dark' : 'light'}/${item.icon}.svg`}
+              className="w-10"
+            />
             {item.name}
           </Link>
         ))}
